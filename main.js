e04e298105142567ecb69a898a31f9afb3b59ba9
CetO-hub <102421594+CetO-hub@users.noreply.github.com>
2023-04-20 15:02:05 +0200

    feat: add mutate function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
+  const dnaBases = ["A", "T", "C", "G"];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
@@ -13,10 +13,31 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+const pAequorFactory = (number, dna) => {
+  // Create object property
+  return {
+    number,
+    dna,
+
+    // Search for a random base and replace by one of the other 3 bases
+    mutate() {
+      let dnaBases = ["A", "T", "C", "G"];
+
+      let randomIndexNumber = Math.floor(Math.random() * 15);
+      
+      let randomDNABase = this.dna[randomIndexNumber];
+      let randomNewDNABase = dnaBases.filter((base) => base !== randomDNABase)[
+        Math.floor(Math.random() * 3)
+      ];
+      this.dna.splice(randomIndexNumber, 1, randomNewDNABase);
+      
+      return dna;
+    },
+  };
+};
 
 
+let candida = pAequorFactory(1, mockUpStrand());
 
-
-
-
-
+console.log(candida.dna)
+console.log(candida.mutate())
